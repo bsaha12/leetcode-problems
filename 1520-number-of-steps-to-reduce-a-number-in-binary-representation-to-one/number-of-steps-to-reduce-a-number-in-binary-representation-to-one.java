22c43007ff9f7748c90aaa0fc3bfd43dcb06a334
Bhaskar Saha <bhaskar12jan678@gmail.com>
2024-05-29 22:39:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (43.61%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,36 +1,16 @@
 class Solution {
     public int numSteps(String s) {
-        char[] arr = ('0' + s).toCharArray();
-        int left = 1;
-        int right = arr.length - 1;
-        int steps = 0;
-        while (right - left + 1 > 1) {
-            if (arr[right] == '1') {
-                int carry = 1;
-                for (int i = right; i >= left; i--) {
-                    if (arr[i] == '1' && carry == 1) {
-                        arr[i] = '0';
-                        carry = 1;
-                    } else if (arr[i] == '0' && carry == 0) {
-                        arr[i] = '0';
-                    } else {
-                        arr[i] = '1';
-                        carry = 0;
-                    }
-                }
-                if (carry == 1) {
-                    left--;
-                    arr[left] = '1';
-                }
-                steps++;
-            } else {
-                for (int i = right; i > left; i--) {
-                    arr[i] = arr[i - 1];
-                }
-                left++;
-                steps++;
+        char[] arr = s.toCharArray() ;
+        int carry = 0;
+        int steps = 0 ;
+        for(int i = arr.length - 1 ; i > 0 ; i--){
+            if(  arr[i] - '0' + carry == 1 ){
+                steps += 2;
+                carry = 1 ;
+            }else{
+                steps++ ;
             }
         }
-        return steps;
-    }//
+        return steps + carry ;
+    }
 }
