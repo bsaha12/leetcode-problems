4fba62fc5a31396ae34831fff1736210e1ecf7aa
Bhaskar Saha <bhaskar12jan678@gmail.com>
2024-06-01 05:58:17 +0530

    Time: 1 ms (99.87%) | Memory: 41.9 MB (41.24%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int result = 0 ;
+        for(int i = 1 ; i < s.length() ; i++ ){
+            result += Math.abs((int) s.charAt(i) - (int)s.charAt(i - 1)  ) ;
+        }
+        return result ;
+    }
+}
