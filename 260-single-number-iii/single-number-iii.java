11f54f2d4531b69d89b42e826efdcb0aeeac44d2
Bhaskar Saha <bhaskar12jan678@gmail.com>
2024-06-01 20:21:16 +0530

    Time: 1 ms (100.00%) | Memory: 45.9 MB (12.54%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -1,20 +1,53 @@
+// class Solution {
+//     public int[] singleNumber(int[] nums) {
+//         if(n)
+//         int xor = 0 ;
+//         for(int num : nums){
+//             xor = xor^num  ;
+//         }
+
+//         int shift_position = 0 ;
+//         while( ((xor >> shift_position ) & 1) != 1   ){
+//             shift_position++ ;
+//         }
+
+//         int num1 = 0 ;
+//         int num2 = 0 ;
+//         for(int num : nums){
+//             if( ((num1 >> shift_position) & 1) != 1  ){
+//                 num1 = num1 ^ num ;
+//             }else{
+//                 num2 = num2 ^ num ;
+//             }
+//         }
+
+//         return new int[]{num1 , num2} ;
+//     }
+// }
+
 class Solution {
     public int[] singleNumber(int[] nums) {
-        if(nums.length == 2) return nums ;
-        Set<Integer> set = new HashSet<>() ;
-        for(int num : nums){
-            if(set.contains(num) ){
-                set.remove(num) ;
-            }else{
-                set.add(num) ;
+        int overall_xor = 0;
+        for (int n : nums) {
+            overall_xor ^= n;
+        }
+
+        int first_group_xor = 0;
+        int second_group_xor = 0;
+
+        int bit_pos_dif = 0;
+        while (((overall_xor >> bit_pos_dif) & 1) != 1) {
+            bit_pos_dif++;
+        }
+
+        for (int num : nums) {
+            if (((num >> bit_pos_dif) & 1) == 1) {
+                first_group_xor ^= num;
+            } else {
+                second_group_xor ^= num;
             }
         }
-        int i = 0 ;
-        int[] result = new int[2] ;
-        for(int num : set){
-            result[i++] = num ;
-        }
-        return result ;
 
-    }//
+        return new int[] {first_group_xor, second_group_xor};
+    }
 }
