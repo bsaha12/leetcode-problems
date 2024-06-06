107fe82466fb52713d48ed894674d7e8b8adb871
Bhaskar Saha <bhaskar12jan678@gmail.com>
2024-06-06 22:43:42 +0530

    Time: 584 ms (6.22%) | Memory: 53 MB (98.76%) - LeetSync

--- 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.java
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.java
@@ -1,37 +1,34 @@
 class Solution {
     public boolean isPossibleDivide(int[] nums, int k) {
-       Arrays.sort(nums) ;
-        for(int i = 0 ; i < nums.length ; i++){
-            if(nums[i] > 0 ){
-                if(!isPossible(nums , i , k)){
-                    return false ;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                if (!isPossible(nums, i, k)) {
+                    return false;
                 }
             }
         }
-        return true ;
+        return true;
     }//
-    public boolean isPossible(int[] nums , int i , int k ){
-        int prev = nums[i] ;
-        nums[i] = 0 ;
-        i++ ;
-        int count = 1 ;
-        while(i < nums.length && count < k ){
-            if(nums[i] != 0 && nums[i] != prev  ){
-                if(nums[i] ==  prev + 1  ){
-                    nums[i] = 0 ;
-                    count++ ;
-                    prev++ ;
-                }else{
-                    return false ;
-                }
+
+    public boolean isPossible(int[] nums, int i, int k) {
+        int curr = nums[i] + 1;
+        nums[i] = 0;
+        i++;
+        int count = 1;
+        while (i < nums.length && count < k) {
+            if (nums[i] == curr) {
+                nums[i] = 0;
+                count++;
+                curr++;
             }
-            i++ ;
+            i++;
         }
 
-        if(count == k){
-            return true ;
+        if (count == k) {
+            return true;
         }
 
-        return false ;
+        return false;
     }//
 }
