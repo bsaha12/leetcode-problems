09d352492e903da5056834d52cfe1017bc4821e6
Bhaskar Saha <bhaskar12jan678@gmail.com>
2024-06-14 08:49:18 +0530

    Time: 37 ms (72.13%) | Memory: 60.3 MB (31.15%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
@@ -2,7 +2,6 @@ class Solution {
     public int minIncrementForUnique(int[] nums) {
         Arrays.sort(nums) ;
         int count = 0;
-        Set<Integer> set = new HashSet<>();
         int nextAvailable = 0 ;
         for(int num : nums){
             if(num < nextAvailable){
