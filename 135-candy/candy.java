1ff87ebe7d6cd4a4941456a63cf580cbb048876e
Bhaskar Saha <bhaskar12jan678@gmail.com>
2024-06-15 22:27:17 +0530

    Time: 3 ms (82.48%) | Memory: 45.6 MB (36.37%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,29 +1,29 @@
 class Solution {
     public int candy(int[] ratings) {
-      int n = ratings.length ;
-      int count = n , i = 1 ;
-      while(i < n){
-        if(ratings[i] == ratings[i - 1] ){
-            i++ ;
-             continue ;
-        }
+        int n = ratings.length;
+        int count = n, i = 1;
+        while (i < n) {
+            if (ratings[i] == ratings[i - 1]) {
+                i++;
+                continue;
+            }
 
-        int peak = 0 ;
-        while(i < n && ratings[i] > ratings[i - 1] ){
-            peak++ ;
-            i++ ;
-            count += peak ;
-        }
+            int peak = 0;
+            while (i < n && ratings[i] > ratings[i - 1]) {
+                peak++;
+                i++;
+                count += peak;
+            }
 
-        int valley = 0;
-        while(i < n && ratings[i] < ratings[ i - 1] ){
-            valley++ ;
-            i++ ;
-            count += valley ;
-        }
+            int valley = 0;
+            while (i < n && ratings[i] < ratings[i - 1]) {
+                valley++;
+                i++;
+                count += valley;
+            }
 
-        count -= Math.min(peak , valley) ;
-      }  
-      return count ;
+            count -= Math.min(peak, valley);
+        }
+        return count;
     }
 }
