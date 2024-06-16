566b9a1506ee37964095df6b2b381ecf89260e4d
Bhaskar Saha <bhaskar12jan678@gmail.com>
2024-06-16 12:56:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (63.40%) - LeetSync

---
+++ 330-patching-array/patching-array.java
@@ -0,0 +1,36 @@
+// class Solution {
+//     public int minPatches(int[] nums, int n) {
+//        int missing = 1 ;
+//        int patches = 0 ;
+//        int i = 0 ;
+//        while(missing <= n   ){
+//          if(i < nums.length && nums[i] <= missing ){
+//             missing += nums[i] ;
+//             i++ ;
+//          }else{
+//             missing += missing ;
+//             patches++ ;
+//          }
+//        }
+//        return patches ; 
+//     }
+// }
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        long missing = 1;
+        int patches = 0;
+        int index = 0;
+
+        while (missing <= n) {
+            if (index < nums.length && nums[index] <= missing) {
+                missing += nums[index];
+                index++;
+            } else {
+                missing += missing;
+                patches++;
+            }
+        }
+
+        return patches;
+    }
+}
