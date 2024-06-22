cb7303d3984cfa6f94aed62c5ee91c53267264f8
Bhaskar Saha <bhaskar12jan678@gmail.com>
2024-06-22 21:49:39 +0530

    Time: 4 ms (52.03%) | Memory: 43.3 MB (21.78%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -13,45 +13,22 @@ class Solution {
         return list;
     }//////
 
-    void helper(int i, int target) {
-        // base case
+    void helper(int start, int target) {
+        // base conditions
+        if (target < 0)
+            return;
         if (target == 0) {
-            List<Integer> temp = new ArrayList<>();
-            for (Integer n : path) {
-                temp.add(n);
-            }
-            list.add(temp);
-            return ;
-        }
-        if(target < 0){
-            return ;
+            list.add(new ArrayList(path));
+            return;
         }
 
-        // actual function logic
-        int len = arr.length;
+        // actual body
+        for (int i = start; i < arr.length; i++) {
+            if (i > start && arr[i] == arr[i - 1])
+                continue;
 
-        // skip duplicates
-        while (i != 0 && i < len && arr[i] == arr[i - 1]) {
-            i++;
-        }
-
-        while (i < len) {
-            int currTarget = target ;
             path.add(arr[i]);
-            currTarget -= arr[i] ;
-            helper(i + 1, currTarget );
-            i++;
-            int dups = 0;
-            while (i < len && arr[i] == arr[i - 1]) {
-                path.add(arr[i]);
-                currTarget -= arr[i] ;
-                helper(i + 1, currTarget);
-                dups++;
-                i++;
-            }
-            while (dups-- > 0) {
-                path.remove(path.size() - 1);
-            }
+            helper(i + 1, target - arr[i]);
             path.remove(path.size() - 1);
         }
 
