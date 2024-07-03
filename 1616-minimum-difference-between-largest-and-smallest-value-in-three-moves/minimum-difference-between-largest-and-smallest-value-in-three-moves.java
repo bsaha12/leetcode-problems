e7a008939a31a7ed2303407aca808ab3bcec80b4
Bhaskar Saha <bhaskar12jan678@gmail.com>
2024-07-03 20:52:50 +0530

    Time: 31 ms (12.34%) | Memory: 55 MB (95.28%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -1,14 +1,33 @@
 class Solution {
     public int minDifference(int[] nums) {
-        Arrays.sort(nums) ;
         if(nums.length <= 4) return 0 ;
-        //check all possibilities(total 4)
+        //Keep smallest 4 numbers
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((x , y) -> y - x) ;
+        for(Integer num : nums){
+            maxHeap.offer(num) ;
+            if(maxHeap.size() > 4 ){
+                maxHeap.poll() ;
+            }
+        }
+        List<Integer> small = new ArrayList<>(maxHeap) ;
+        Collections.sort(small) ;
+
+        //Keep largest 4 numbers
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>() ;
+        for(Integer num : nums){
+            minHeap.offer(num) ;
+            if(minHeap.size() > 4 ){
+                minHeap.poll() ;
+            }
+        }
+        List<Integer> large = new ArrayList<>(minHeap) ;
+        Collections.sort(large) ;
+
         int min = Integer.MAX_VALUE ;
-        int n = nums.length ;
-        min = Math.min(min ,nums[n- 1] -  nums[3] ) ;
-        min = Math.min(min ,nums[n- 4] -  nums[0] ) ;
-        min = Math.min(min ,nums[n- 2] -  nums[2] ) ;
-        min = Math.min(min ,nums[n- 3] -  nums[1] ) ;
+        for(int i = 0 ; i < 4 ; i++){
+            min = Math.min(min , large.get(i) - small.get(i) ) ;
+        }
         return min ;
+
     }
 }
