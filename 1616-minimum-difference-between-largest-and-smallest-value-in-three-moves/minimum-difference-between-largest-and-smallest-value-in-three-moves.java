37d24ac2bda21e6e587dc472c1cbd056d962bed0
Bhaskar Saha <bhaskar12jan678@gmail.com>
2024-07-03 20:54:52 +0530

    Time: 33 ms (10.76%) | Memory: 55.6 MB (88.98%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -1,33 +1,77 @@
-class Solution {
+// class Solution {
+//     public int minDifference(int[] nums) {
+//         if(nums.length <= 4) return 0 ;
+//         //Keep smallest 4 numbers
+//         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((x , y) -> y - x) ;
+//         for(Integer num : nums){
+//             maxHeap.offer(num) ;
+//             if(maxHeap.size() > 4 ){
+//                 maxHeap.poll() ;
+//             }
+//         }
+//         List<Integer> small = new ArrayList<>(maxHeap) ;
+//         Collections.sort(small) ;
+
+//         //Keep largest 4 numbers
+//         PriorityQueue<Integer> minHeap = new PriorityQueue<>() ;
+//         for(Integer num : nums){
+//             minHeap.offer(num) ;
+//             if(minHeap.size() > 4 ){
+//                 minHeap.poll() ;
+//             }
+//         }
+//         List<Integer> large = new ArrayList<>(minHeap) ;
+//         Collections.sort(large) ;
+
+//         int min = Integer.MAX_VALUE ;
+//         for(int i = 0 ; i < 4 ; i++){
+//             min = Math.min(min , large.get(i) - small.get(i) ) ;
+//         }
+//         return min ;
+
+//     }
+// }
+public class Solution {
+
     public int minDifference(int[] nums) {
-        if(nums.length <= 4) return 0 ;
-        //Keep smallest 4 numbers
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((x , y) -> y - x) ;
-        for(Integer num : nums){
-            maxHeap.offer(num) ;
-            if(maxHeap.size() > 4 ){
-                maxHeap.poll() ;
+        int numsSize = nums.length;
+        if (numsSize <= 4) {
+            return 0;
+        }
+
+        // Find the four smallest elements using a fixed-size max heap
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(
+            Collections.reverseOrder()
+        );
+        for (int num : nums) {
+            maxHeap.offer(num);
+            if (maxHeap.size() > 4) {
+                maxHeap.poll();
             }
         }
-        List<Integer> small = new ArrayList<>(maxHeap) ;
-        Collections.sort(small) ;
+        List<Integer> smallestFour = new ArrayList<>(maxHeap);
+        Collections.sort(smallestFour);
 
-        //Keep largest 4 numbers
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>() ;
-        for(Integer num : nums){
-            minHeap.offer(num) ;
-            if(minHeap.size() > 4 ){
-                minHeap.poll() ;
+        // Find the four largest elements using a fixed-size min heap
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            minHeap.offer(num);
+            if (minHeap.size() > 4) {
+                minHeap.poll();
             }
         }
-        List<Integer> large = new ArrayList<>(minHeap) ;
-        Collections.sort(large) ;
+        List<Integer> largestFour = new ArrayList<>(minHeap);
+        Collections.sort(largestFour);
 
-        int min = Integer.MAX_VALUE ;
-        for(int i = 0 ; i < 4 ; i++){
-            min = Math.min(min , large.get(i) - small.get(i) ) ;
+        int minDiff = Integer.MAX_VALUE;
+        // Four scenarios to compute the minimum difference
+        for (int i = 0; i < 4; i++) {
+            minDiff = Math.min(
+                minDiff,
+                largestFour.get(i) - smallestFour.get(i)
+            );
         }
-        return min ;
 
+        return minDiff;
     }
 }
