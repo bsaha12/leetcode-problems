dd39b00c69428e678ff0504e0ddf2553dbc87d8d
Bhaskar Saha <bhaskar12jan678@gmail.com>
2024-07-04 09:04:34 +0530

    Time: 5 ms (84.77%) | Memory: 81.8 MB (56.71%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.java
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.java
@@ -1,35 +1,30 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-        List<Integer> list = new ArrayList<>() ;
-        int sum = 0 ;
-        head = head.next ;
-        while(head != null){
-            if(head.val != 0){
-                sum += head.val ;
-            }else{
-                list.add(sum) ;
+        ListNode result = new ListNode();
+        ListNode curr = result;
+        int sum = 0;
+        head = head.next;
+        while (head != null) {
+            if (head.val != 0) {
+                sum += head.val;
+            } else {
+                ListNode temp = new ListNode(sum);
+                curr.next = temp;
+                curr = temp;
                 sum = 0;
             }
-            head = head.next ;
+            head = head.next;
         }
-
-        ListNode result = new ListNode() ;
-        ListNode curr = result ;
-        for(Integer num : list){
-            ListNode temp = new ListNode(num) ;
-            curr.next = temp ;
-            curr = temp ;
-        }
-        return result.next ;
+        return result.next;
     }///
 }
