3c95ed941627e419906274f8df5b630589e82c8c
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-01-09 20:25:46 +0530

    Time: 45 ms (40.29%) | Memory: 57.4 MB (76.69%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,26 +1,72 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         int maxLen = 0 ;
-        Set<Integer> set = new HashSet<>() ;
+        Map<Integer , Integer> map = new HashMap<>() ;
+        int i = 0 ;
+        UF uf = new UF(nums.length) ;
         for(int num : nums){
-            set.add(num) ;
+            if(map.containsKey(num)){
+                continue ;
+            }
+
+            if(map.containsKey(num - 1)){
+                uf.union(i , map.get(num-1)) ;
+            }
+
+            if(map.containsKey(num + 1)){
+                uf.union(i , map.get(num + 1)) ;
+            }
+
+            map.put(num , i) ;
+            i++ ;
         }
-        for(int num : nums){
-            int currSequence = 1 ;
-            int prev = num - 1 ;
-            int next = num + 1 ;
-            while(set.remove(prev)){
-                currSequence++ ;
-                prev-- ;
-            }
-            while(set.remove(next)){
-                currSequence++ ;
-                next++ ;
-            }
-            if(currSequence > maxLen){
-                maxLen = currSequence;
+
+        return uf.findLargestSetSize() ;
+    }
+}
+
+class UF{
+    int[] size , parent ;
+    public UF(int len){
+        size = new int[len] ;
+        parent = new int[len] ;
+        for(int i = 0 ; i < len ; i++){
+            parent[i] = i ;
+            size[i] = 1 ;
+        }
+    }
+
+    public void union(int x , int y){
+        int rpsX = find(x) ;
+        int rpsY = find(y) ;
+
+        if(rpsX == rpsY){
+            return ;
+        }
+
+        if(size[rpsX] > size[rpsY]){
+            parent[rpsY] = rpsX ;
+            size[rpsX] += size[rpsY] ;
+        }else{
+            parent[rpsX] = rpsY ;
+            size[rpsY] += size[rpsX] ;
+        }
+    }
+
+    public int find(int x){
+        if(x != parent[x]){
+            parent[x] = find(parent[x]) ;
+        }
+        return parent[x] ;
+    }
+
+    public int findLargestSetSize(){
+        int maxSize = 0 ;
+        for(int i = 0 ; i < size.length ; i++){
+            if(parent[i] == i){
+                maxSize = Math.max(maxSize , size[i]) ;
             }
         }
-        return maxLen ;
+        return maxSize ;
     }
 }
