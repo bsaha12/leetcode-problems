517817c3cb287520ae3e26a3a113817b2ec56f57
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-01-13 20:15:00 +0530

    Time: 355 ms (40.17%) | Memory: 63.2 MB (36.88%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,76 +1,75 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        int nodesCount = points.length ;
-        List<Edge> edges = new ArrayList<>() ;
-        UF uf = new UF(nodesCount) ;
-        for(int i = 0  ; i < nodesCount ; i++){
-            for(int j = 0 ; j < nodesCount ; j++){
-                if(i != j){
-                    Edge edge = new Edge(i , j , points[i] , points[j]) ;
-                    edges.add(edge) ;
-                }
+        int nodesCount = points.length;
+        List<Edge> edges = new ArrayList<>();
+        UF uf = new UF(nodesCount);
+        for (int i = 0; i < nodesCount; i++) {
+            for (int j = i + 1; j < nodesCount; j++) {
+                Edge edge = new Edge(i, j, points[i], points[j]);
+                edges.add(edge);
             }
         }
-        Collections.sort(edges , (x , y ) -> Integer.compare(x.weight , y.weight)) ;
-        int sum = 0 ;
-        for(Edge edge : edges){
-            int start = edge.start ;
-            int end = edge.end ;
-            int weight = edge.weight ;
-            if(uf.union(start , end)){
-                sum += weight ;
+        Collections.sort(edges, (x, y) -> Integer.compare(x.weight, y.weight));
+        int sum = 0;
+        for (Edge edge : edges) {
+            int start = edge.start;
+            int end = edge.end;
+            int weight = edge.weight;
+            if (uf.union(start, end)) {
+                sum += weight;
             }
         }
 
-        return sum ;
+        return sum;
     }
 }
 
-class UF{
-    int[] parent , size ;
+class UF {
+    int[] parent, size;
 
-    public UF(int len){
-        parent = new int[len] ;
-        size = new int[len] ;
-        for(int i = 0 ; i < len ; i++){
-            parent[i] = i ;
-            size[i] = 1 ;
+    public UF(int len) {
+        parent = new int[len];
+        size = new int[len];
+        for (int i = 0; i < len; i++) {
+            parent[i] = i;
+            size[i] = 1;
         }
     }
 
-    public boolean union(int x , int y){
-        int rpsX = find(x) ;
-        int rpsY = find(y) ;
+    public boolean union(int x, int y) {
+        int rpsX = find(x);
+        int rpsY = find(y);
 
-        if(rpsX == rpsY){
-            return false ;
+        if (rpsX == rpsY) {
+            return false;
         }
 
-        if(size[rpsX] > size[rpsY]){
-            size[rpsX] += size[rpsY] ;
-            parent[rpsY] = rpsX ;
-        }else{
-            size[rpsY] += size[rpsX] ;
-            parent[rpsX] = rpsY ;
+        if (size[rpsX] > size[rpsY]) {
+            size[rpsX] += size[rpsY];
+            parent[rpsY] = rpsX;
+        } else {
+            size[rpsY] += size[rpsX];
+            parent[rpsX] = rpsY;
         }
-        return true ;
+        return true;
     }
 
-    public int find(int x){
-        if(x != parent[x]){
-            parent[x] = find(parent[x]) ;
+    public int find(int x) {
+        if (x != parent[x]) {
+            parent[x] = find(parent[x]);
         }
-        return parent[x] ;
+        return parent[x];
     }
 
 }
 
 class Edge {
-    int start , end ;
-    int weight ;
-    public Edge(int startIndex ,int endIndex ,int[] startCoords ,int[] endCoords ){
-        start = startIndex ;
-        end = endIndex  ;
-        weight = Math.abs(startCoords[0] - endCoords[0] ) + Math.abs(startCoords[1] - endCoords[1]) ;
+    int start, end;
+    int weight;
+
+    public Edge(int startIndex, int endIndex, int[] startCoords, int[] endCoords) {
+        start = startIndex;
+        end = endIndex;
+        weight = Math.abs(startCoords[0] - endCoords[0]) + Math.abs(startCoords[1] - endCoords[1]);
     }
 }
