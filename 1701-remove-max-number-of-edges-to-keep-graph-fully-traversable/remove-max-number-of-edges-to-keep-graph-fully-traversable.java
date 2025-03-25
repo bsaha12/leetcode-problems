05643b71c6e5eb153f783dafe7b9927a4f54ba91
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-03-25 09:06:13 +0530

    Time: 26 ms (44.41%) | Memory: 104.8 MB (64.03%) - LeetSync

--- 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.java
+++ 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.java
@@ -1,76 +1,104 @@
 class Solution {
-    class UnionFind {
-        int[] parent;
-        int[] rank;
-        int count;
-        public UnionFind(int n) {
-            parent = new int[n];
-            rank = new int[n];
-            count = n;
-            for (int i = 0; i < n; i++) {
-                parent[i] = i;
-                rank[i] = 0;
+    public int maxNumEdgesToRemove(int n, int[][] edges) {
+        // sorting
+        Arrays.sort(edges , (int[] a1 , int[] a2) -> {
+            if(a1[0] == 3){
+                return -1 ;
             }
-        }
-
-        public int find(int x) {
-            if (parent[x] != x) {
-                parent[x] = find(parent[x]);
+            if(a2[0] == 3){
+                return 1 ;
             }
-            return parent[x];
-        }
+            return 0 ;
+        } );
 
-        public boolean union(int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
+        // intialize
+        UnionBySize uA = new UnionBySize(n+1) ;
+        UnionBySize uB = new UnionBySize(n+1) ;
+        int connectedComponentsA = n ;
+        int connectedComponentsB = n ;
+        int remove = 0 ;
 
-            if (rootX != rootY) {
-                if (rank[rootX] > rank[rootY]) {
-                    parent[rootY] = rootX;
-                } else if (rank[rootX] < rank[rootY]) {
-                    parent[rootX] = rootY;
-                } else {
-                    parent[rootY] = rootX;
-                    rank[rootX]++;
+        for(int[] arr : edges){
+            int type = arr[0] ;
+            int u = arr[1] ;
+            int v = arr[2] ;
+
+            if(type == 3){
+                boolean mergeA = uA.union(u ,v) ;
+                boolean mergeB = uB.union(u, v) ;
+
+                if(mergeA){
+                    connectedComponentsA-- ;
                 }
-                count--;
-                return true;
+
+                if(mergeB){
+                    connectedComponentsB-- ;
+                }
+
+                if(mergeA == false && mergeB == false){
+                    remove++ ;
+                }
+            }else if(type == 1 ){
+                boolean mergeA = uA.union(u,v) ;
+                if(mergeA){
+                    connectedComponentsA-- ;
+                }else{
+                    remove++ ;
+                }
+
+            }else{
+                boolean mergeB = uB.union(u , v);
+                if(mergeB){
+                    connectedComponentsB-- ;
+                }else{
+                    remove++ ;
+                }
             }
+        }//
+
+        if( connectedComponentsA != 1 || connectedComponentsB != 1){
+            return -1 ;
+        }
+
+        return remove ;
+    }
+}
+
+class UnionBySize{
+    int[] parent , size ;
+    // constructor
+    public UnionBySize(int n){
+        size = new int[n] ;
+        parent = new int[n] ;
+        for(int i = 0 ; i < n ; i++){
+            size[i] = 1 ;
+            parent[i] = i ;
+        }
+    }
+    // union
+    public boolean union(int u , int v){
+        int rpsU = find(u) ;
+        int rpsV = find(v) ;
+
+        if(rpsU == rpsV){
             return false;
         }
-        public int getCount() {
-            return count;
-        }
-    }
-    public int maxNumEdgesToRemove(int n, int[][] edges) {
-        UnionFind aliceUF = new UnionFind(n);
-        UnionFind bobUF = new UnionFind(n);
-        int addedEdges = 0;
-        Arrays.sort(edges, (a, b) -> b[0] - a[0]);
-        for (int[] edge : edges) {
-            int type = edge[0];
-            int u = edge[1] - 1;
-            int v = edge[2] - 1;
 
-            if (type == 3) {
-                boolean aliceConnected = aliceUF.union(u, v);
-                boolean bobConnected = bobUF.union(u, v);
-                if (aliceConnected || bobConnected) {
-                    addedEdges++;
-                }
-            } else if (type == 1) {
-                if (aliceUF.union(u, v)) {
-                    addedEdges++;
-                }
-            } else if (type == 2) {
-                if (bobUF.union(u, v)) {
-                    addedEdges++;
-                }
-            }
+        if(size[rpsU] > size[rpsV]){
+            parent[rpsV] = rpsU ;
+            size[rpsU] += size[rpsV] ;
+        }else{
+            parent[rpsU] = rpsV ;
+            size[rpsV] += size[rpsU] ;
         }
-        if (aliceUF.getCount() > 1 || bobUF.getCount() > 1) {
-            return -1;
-        }
-        return edges.length - addedEdges;
+
+        return true ;
     }
-	}
+    // find
+    public int find(int x){
+        if(x != parent[x]){
+            parent[x] = find(parent[x]) ;
+        }
+        return parent[x] ;
+    }
+}
