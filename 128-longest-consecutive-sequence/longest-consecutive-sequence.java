7edb6ef1e5876fae86532315c3cb1894e97915d9
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-03-31 08:55:46 +0530

    Time: 46 ms (14.35%) | Memory: 74.1 MB (5.27%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,72 +1,27 @@
 class Solution {
+    Set<Integer> set ;
     public int longestConsecutive(int[] nums) {
-        int maxLen = 0 ;
-        Map<Integer , Integer> map = new HashMap<>() ;
-        int i = 0 ;
-        UF uf = new UF(nums.length) ;
+        set = new HashSet<>() ;
         for(int num : nums){
-            if(map.containsKey(num)){
-                continue ;
-            }
-
-            if(map.containsKey(num - 1)){
-                uf.union(i , map.get(num-1)) ;
-            }
-
-            if(map.containsKey(num + 1)){
-                uf.union(i , map.get(num + 1)) ;
-            }
-
-            map.put(num , i) ;
-            i++ ;
+            set.add(num) ;
         }
 
-        return uf.findLargestSetSize() ;
-    }
-}
-
-class UF{
-    int[] size , parent ;
-    public UF(int len){
-        size = new int[len] ;
-        parent = new int[len] ;
-        for(int i = 0 ; i < len ; i++){
-            parent[i] = i ;
-            size[i] = 1 ;
+        int longestSeq = 0 ;
+        for(int num : nums){
+            longestSeq = Math.max(findSeqWithNumber(num) , longestSeq) ;
         }
-    }
+        return longestSeq ;
+    }//
 
-    public void union(int x , int y){
-        int rpsX = find(x) ;
-        int rpsY = find(y) ;
-
-        if(rpsX == rpsY){
-            return ;
+    public int findSeqWithNumber(int num){
+        if(!set.contains(num)){
+            return 0 ;
         }
 
-        if(size[rpsX] > size[rpsY]){
-            parent[rpsY] = rpsX ;
-            size[rpsX] += size[rpsY] ;
-        }else{
-            parent[rpsX] = rpsY ;
-            size[rpsY] += size[rpsX] ;
-        }
-    }
+        set.remove(num) ;
+        int rightCount = findSeqWithNumber(num+1) ;
+        int leftCount = findSeqWithNumber(num-1) ; 
 
-    public int find(int x){
-        if(x != parent[x]){
-            parent[x] = find(parent[x]) ;
-        }
-        return parent[x] ;
-    }
-
-    public int findLargestSetSize(){
-        int maxSize = 0 ;
-        for(int i = 0 ; i < size.length ; i++){
-            // if(parent[i] == i){
-                maxSize = Math.max(maxSize , size[i]) ;
-            // }
-        }
-        return maxSize ;
-    }
+        return rightCount + leftCount + 1 ;
+    }//
 }
