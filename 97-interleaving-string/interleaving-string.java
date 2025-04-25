bc8d16006e698fee1c2e7c8b8a3aade0fffc275c
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-04-25 09:04:44 +0530

    Time: 1 ms (89.80%) | Memory: 41.7 MB (70.61%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,46 +1,43 @@
 class Solution {
     char[] str1 , str2 , str3 ;
-    int[][][] dp ;
+    int[][] dp ;
     public boolean isInterleave(String s1, String s2, String s3) {
-        str1 = s1.toCharArray() ;
-        str2 = s2.toCharArray() ;
-        str3 = s3.toCharArray() ;
-
-        if(str1.length + str2.length != str3.length) return false ;
-
-        String s4 = s1 + s2 ;
-        String s5 = s1 + s2 ;
-        if(s3.equals(s4) || s3.equals(s5)) return true ;
-
-        dp = new int[str1.length + 1][str2.length + 1][str3.length + 1] ;
-
-        return helper(0 , 0 , 0) ;
+       int l1 = s1.length() ;
+       int l2 = s2.length() ;
+       int l3 = s3.length() ;
+       if(l1 + l2 != l3) return false ;
+       str1 = s1.toCharArray() ;
+       str2 = s2.toCharArray() ;
+       str3 = s3.toCharArray() ;
+       dp = new int[ l1 + 1 ][ l2+ 2] ;
+       for(int[] arr : dp){
+        Arrays.fill(arr , -1) ;
+       }
+       return helper( 0 , 0)  ;
     }
 
-    public boolean helper(int i , int j , int k){
-        if(i >= str1.length && j >= str2.length){
-            if(k == str3.length) return true ;
-            else return false ;
+    public boolean helper(int i , int j){
+        int k = i + j ;
+        if(k == str3.length) {
+            return i == str1.length && j == str2.length ;
         }
 
-        if( k >= str3.length) return false ;
+        if(dp[i][j] != -1){
+            return dp[i][j] == 1 ? true : false ;
+        }
 
+        boolean first = false , second = false ; 
+        if( i < str1.length && k < str3.length && str1[i] == str3[k] ){
+            first = helper(i + 1 , j) ;
+        }
 
-        if(dp[i][j][k] != 0) return dp[i][j][k] == 1 ? true : false ;
+        if(!first && j < str2.length && k < str3.length && str2[j] == str3[k] ){
+            second = helper(i , j + 1) ;
+        }
 
-        char ch1 = i == str1.length ? '!' : str1[i] ;
-        char ch2 = j == str2.length ? '!' : str2[j] ;
-        char ch3 = str3[k] ;
+        dp[i][j] = first || second ? 1 : 0 ;
 
-       boolean result = false ;
-       if(ch1 != ch3 && ch2 != ch3) result =  false ;
-       else if(ch1 == ch3 && ch2 == ch3) result = helper(i+1,j,k+1)||helper(i,j+1,k+1);
-       else if(ch1 == ch3) result = helper(i+1,j,k+1) ;
-       else result = helper(i,j+1,k+1) ;
-
-       dp[i][j][k] = result ? 1 : -1 ;
-
-       return result ;
+        return first || second;
 
     }
 }
