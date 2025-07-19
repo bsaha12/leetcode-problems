21dd9098911edb0c395310539b37fa544f93b916
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-07-19 23:08:39 +0530

    Time: 46 ms (56.12%) | Memory: 48.6 MB (43.27%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,51 +1,56 @@
 class Solution {
-    Boolean[][] dp ;
-    int[] arr ;
+    Boolean[][] dp;
+    int[] arr;
 
     public boolean canPartition(int[] nums) {
-        int sum = Arrays.stream(nums).sum() ;
-        if(sum % 2 != 0) return false ;
-        arr = nums ;
-        dp = new Boolean[nums.length + 1][sum + 1] ;
-        return helper(0 , sum / 2) ;
+        int sum = Arrays.stream(nums).sum();
+        if (sum % 2 != 0)
+            return false;
+        // arr = nums ;
+        // dp = new Boolean[nums.length + 1][sum + 1] ;
+        // return helper(0 , sum / 2) ;
 
-        // int n = nums.length ;
-        // boolean[][] dp = new boolean[n + 1][sum + 1] ;
+        sum = sum / 2; // target sum is half the value
+        int n = nums.length;
+        boolean[][] dp = new boolean[n + 1][sum + 1];
 
-        // // initialize 
-        // // i = n --> false , sum = 0 -> true;
-        // for(int i = 0 ; i <= n ; i++){
-        //     dp[i][0] = true ;
-        // }
+        // initialize 
+        // i = n --> false , sum = 0 -> true; (sum = 0 will override i = n)
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
 
-        // // source -> ( n , 0) ;
-        // // target -> (0 , sum) ;
+        // source -> ( n , 0) ;
+        // target -> (0 , sum) ;
 
-        // for(int i = n - 1 ; i >= 0 ; i--){
-        //     for(int j = 1 ; j <= sum ; j++){
-        //         // if(nums[i] > j) {
-        //         //     dp[i][j] = dp[i+1][j] ;
-        //         // }else{
-        //             dp[i][j] = dp[i+1][sum - nums[i]] || dp[i+1][j] ;
-        //         // }
-        //     }
-        // }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 1; j <= sum; j++) {
+                if (nums[i] > j) {
+                    dp[i][j] = dp[i + 1][j];
+                } else {
+                    dp[i][j] = dp[i + 1][j - nums[i]] || dp[i + 1][j];
+                }
+            }
+        }
 
-        // return dp[0][sum] ;
-        
+        return dp[0][sum];
+
     }
 
-    public boolean helper(int i , int sum ){
-        if(sum == 0) return true ;
-        if(i == arr.length) return false ;
+    public boolean helper(int i, int sum) {
+        if (sum == 0)
+            return true;
+        if (i == arr.length)
+            return false;
 
-        if(dp[i][sum] != null) return dp[i][sum] ;
+        if (dp[i][sum] != null)
+            return dp[i][sum];
 
-        if(arr[i] > sum) dp[i][sum] =  helper( i + 1 , sum) ;
-        else dp[i][sum] = helper( i + 1 , sum) || helper( i + 1 , sum - arr[i]) ;
+        if (arr[i] > sum)
+            dp[i][sum] = helper(i + 1, sum);
+        else
+            dp[i][sum] = helper(i + 1, sum) || helper(i + 1, sum - arr[i]);
 
-        // System.out.println(" i :  " + i + ", sum : " + sum + ", result : " + dp[i][sum]) ;
-        
-        return dp[i][sum] ;
+        return dp[i][sum];
     }
 }
