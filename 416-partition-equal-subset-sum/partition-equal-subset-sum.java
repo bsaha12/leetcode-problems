a25fb138256e0b6656d9c525665a3f7fb786a990
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-07-19 22:47:16 +0530

    Time: 55 ms (42.11%) | Memory: 76.4 MB (5.02%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,29 +1,51 @@
 class Solution {
-    int arr[] , total ;
-    Boolean cache[][] ;
+    Boolean[][] dp ;
+    int[] arr ;
 
     public boolean canPartition(int[] nums) {
-        if(nums.length == 1) return false ;
+        int sum = Arrays.stream(nums).sum() ;
+        if(sum % 2 != 0) return false ;
         arr = nums ;
-        total = Arrays.stream(nums).sum() ;
-        if(total == 0) return true ;
-        if(total % 2 != 0) return false ;
+        dp = new Boolean[nums.length + 1][sum + 1] ;
+        return helper(0 , sum / 2) ;
 
-        cache = new Boolean[arr.length + 1][total + 1] ;
-        return helper(0 , 0);
+        // int n = nums.length ;
+        // boolean[][] dp = new boolean[n + 1][sum + 1] ;
+
+        // // initialize 
+        // // i = n --> false , sum = 0 -> true;
+        // for(int i = 0 ; i <= n ; i++){
+        //     dp[i][0] = true ;
+        // }
+
+        // // source -> ( n , 0) ;
+        // // target -> (0 , sum) ;
+
+        // for(int i = n - 1 ; i >= 0 ; i--){
+        //     for(int j = 1 ; j <= sum ; j++){
+        //         // if(nums[i] > j) {
+        //         //     dp[i][j] = dp[i+1][j] ;
+        //         // }else{
+        //             dp[i][j] = dp[i+1][sum - nums[i]] || dp[i+1][j] ;
+        //         // }
+        //     }
+        // }
+
+        // return dp[0][sum] ;
+        
     }
 
-    public boolean helper(int i , int currSum){
-        if( 2*currSum == total) return true ;
-        if( i == arr.length) return false ;
+    public boolean helper(int i , int sum ){
+        if(sum == 0) return true ;
+        if(i == arr.length) return false ;
 
-        if(cache[i][currSum] != null) return cache[i][currSum] ;
+        if(dp[i][sum] != null) return dp[i][sum] ;
 
-        boolean chosen = helper( i + 1 , currSum + arr[i] ) ;
-        boolean notChosen = helper( i + 1 , currSum  ) ;
+        if(arr[i] > sum) dp[i][sum] =  helper( i + 1 , sum) ;
+        else dp[i][sum] = helper( i + 1 , sum) || helper( i + 1 , sum - arr[i]) ;
 
-        cache[i][currSum] = chosen || notChosen ;
-
-        return cache[i][currSum];
+        // System.out.println(" i :  " + i + ", sum : " + sum + ", result : " + dp[i][sum]) ;
+        
+        return dp[i][sum] ;
     }
 }
