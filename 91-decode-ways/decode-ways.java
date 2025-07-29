85f3a150e5af681d943c29c0671fd5e8597915bd
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-07-29 20:32:54 +0530

    Time: 6 ms (5.64%) | Memory: 42.1 MB (36.63%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -4,22 +4,30 @@ class Solution {
 
     public int numDecodings(String s) {
         char[] arr = s.toCharArray();
-        int[] cache = new int[arr.length + 1];
+        // int[] cache = new int[arr.length + 1];
         // Arrays.fill(cache, -1);
         int n = arr.length;
-        cache[n] = 1;
+        // cache[n] = 1;
 
+        int oneStepBefore = 1 ;
+        int twoStepBefore = 1 ;
         for (int i = n - 1; i >= 0; i--) {
             int result = 0;
             if (arr[i] != '0') {
-                result = cache[i + 1];
-                result += i + 1 < arr.length && Integer.parseInt(arr[i] + "" + arr[i + 1]) <= 26 ? cache[i + 2] : 0;
+                // result = cache[i + 1];
+                // result += i + 1 < arr.length && Integer.parseInt(arr[i] + "" + arr[i + 1]) <= 26 ? cache[i + 2] : 0;
+                result = oneStepBefore;
+                result += i + 1 < arr.length && Integer.parseInt(arr[i] + "" + arr[i + 1]) <= 26 ? twoStepBefore : 0;
             }
 
-            cache[i] = result;
+            // cache[i] = result;
+            twoStepBefore = oneStepBefore ;
+            oneStepBefore = result ;
         }
 
-        return cache[0];
+        return oneStepBefore ;
+
+        // return cache[0];
         // return helper(0);
     }
 
