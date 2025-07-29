7513153000eb356bcde8f8665e702b66ffe2512f
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-07-29 10:12:58 +0530

    Time: 6 ms (5.64%) | Memory: 42.1 MB (36.63%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -5,23 +5,38 @@ class Solution {
     public int numDecodings(String s) {
         arr = s.toCharArray();
         cache = new int[arr.length + 1];
-        Arrays.fill(cache, -1);
-        return helper(0);
+        // Arrays.fill(cache, -1);
+        int n = arr.length;
+        cache[n] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int result = 0;
+            if (arr[i] != '0') {
+                result = cache[i + 1];
+                result += i + 1 < arr.length && Integer.parseInt(arr[i] + "" + arr[i + 1]) <= 26 ? cache[i + 2] : 0;
+            }
+
+            cache[i] = result;
+        }
+
+        return cache[0];
+        // return helper(0);
     }
 
-    public int helper(int i) {
-        if( i == arr.length) return 1 ;
+    // function representing number of ways to decode s.substring(i) ;
+    // public int helper(int i) {
+    //     if( i == arr.length) return 1 ;
 
-        if (cache[i] != -1)
-            return cache[i];
+    //     if (cache[i] != -1)
+    //         return cache[i];
 
-        if(arr[i] == '0') return 0 ;
+    //     if(arr[i] == '0') return 0 ;
 
-        int result = helper(i + 1) ;
+    //     int result = helper(i + 1) ;
 
-        result += i+1 < arr.length && Integer.parseInt(arr[i]+""+arr[i+1]) <= 26 ?helper(i + 2) : 0; 
+    //     result += i+1 < arr.length && Integer.parseInt(arr[i]+""+arr[i+1]) <= 26 ?helper(i + 2) : 0; 
 
-        return cache[i] = result;
-    }
+    //     return cache[i] = result;
+    // }
 
 }
