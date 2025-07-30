18b869a296c58b83ce4b409df263b64e182d827f
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-07-30 07:42:37 +0530

    Time: 2 ms (44.47%) | Memory: 41.9 MB (29.01%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -1,38 +1,60 @@
 class Solution {
-    int[] d, c , cache;
+    int[] d, c, cache;
 
     public int mincostTickets(int[] days, int[] costs) {
-        d = days ;
-        c = costs ;
-        cache = new int[days.length + 1] ;
+        d = days;
+        c = costs;
+        cache = new int[days.length + 1];
+        int n = days.length ;
+        cache[n] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int currDay = d[i];
+            int nextIndexIf7DaysChosen = i + 1;
+            int nextIndexIf30DaysChosen = i + 1;
 
+            while (nextIndexIf7DaysChosen < d.length && d[nextIndexIf7DaysChosen] < currDay + 7) {
+                nextIndexIf7DaysChosen++;
+            }
 
-        Arrays.fill(cache , -1) ;
-        return helper(0) ;
-    }
+            while (nextIndexIf30DaysChosen < d.length && d[nextIndexIf30DaysChosen] < currDay + 30) {
+                nextIndexIf30DaysChosen++;
+            }
 
-    public int helper(int i){
-        if(i == d.length) return 0 ;
+            int oneDayChosen = c[0] + cache[i + 1];
+            int sevenDayChosen = c[1] + cache[nextIndexIf7DaysChosen];
+            int thirtyDayChosen = c[2] + cache[nextIndexIf30DaysChosen];
 
-        if(cache[i] != -1) return cache[i] ;
-
-        int currDay = d[i] ;
-        int nextIndexIf7DaysChosen = i + 1 ;
-        int nextIndexIf30DaysChosen = i + 1 ;
-
-        while(nextIndexIf7DaysChosen < d.length && d[nextIndexIf7DaysChosen] < currDay + 7){
-            nextIndexIf7DaysChosen++ ;
+            cache[i] = Math.min(Math.min(oneDayChosen, sevenDayChosen), thirtyDayChosen);
         }
 
-        while(nextIndexIf30DaysChosen < d.length && d[nextIndexIf30DaysChosen] < currDay + 30){
-            nextIndexIf30DaysChosen++ ;
-        }
-
-        int oneDayChosen = c[0] + helper(i+1) ;
-        int sevenDayChosen = c[1] + helper(nextIndexIf7DaysChosen) ;
-        int thirtyDayChosen = c[2] +  helper(nextIndexIf30DaysChosen) ;
-
-        return cache[i] = Math.min(Math.min(oneDayChosen , sevenDayChosen) , thirtyDayChosen ) ;
-
+        return cache[0] ;
+        // Arrays.fill(cache , -1) ;
+        // return helper(0) ;
     }
+
+    // public int helper(int i) {
+    //     if (i == d.length)
+    //         return 0;
+
+    //     if (cache[i] != -1)
+    //         return cache[i];
+
+    //     int currDay = d[i];
+    //     int nextIndexIf7DaysChosen = i + 1;
+    //     int nextIndexIf30DaysChosen = i + 1;
+
+    //     while (nextIndexIf7DaysChosen < d.length && d[nextIndexIf7DaysChosen] < currDay + 7) {
+    //         nextIndexIf7DaysChosen++;
+    //     }
+
+    //     while (nextIndexIf30DaysChosen < d.length && d[nextIndexIf30DaysChosen] < currDay + 30) {
+    //         nextIndexIf30DaysChosen++;
+    //     }
+
+    //     int oneDayChosen = c[0] + helper(i + 1);
+    //     int sevenDayChosen = c[1] + helper(nextIndexIf7DaysChosen);
+    //     int thirtyDayChosen = c[2] + helper(nextIndexIf30DaysChosen);
+
+    //     return cache[i] = Math.min(Math.min(oneDayChosen, sevenDayChosen), thirtyDayChosen);
+    // }
 }
