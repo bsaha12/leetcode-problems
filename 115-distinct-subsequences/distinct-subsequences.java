7686eb40cf47d97fad09fd91dd9dd23138ea27d8
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-08-04 21:55:23 +0530

    Time: 8 ms (96.53%) | Memory: 45.1 MB (88.69%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,38 +1,57 @@
 class Solution {
-    int[][] cache ;
-    char[] sA , tA ;
+    // int[][] cache ;
+    // char[] sA , tA ;
     public int numDistinct(String s, String t) {
-        sA = s.toCharArray() ;
-        tA = t.toCharArray() ;
+        char[] sA = s.toCharArray() ;
+        char[] tA = t.toCharArray() ;
 
         int m = sA.length ;
         int n = tA.length ;
-        cache = new int[m][n] ;
-        for(int[] arr : cache) Arrays.fill(arr , -1) ;
+        // cache = new int[m][n] ;
+        // for(int[] arr : cache) Arrays.fill(arr , -1) ;
+        // return helper(0 , 0) ;
 
-        return helper(0 , 0) ;
-    }
+        int[] prev = new int[n + 1] ;
+        prev[n] = 1;
+        for(int i = m - 1; i >= 0 ; i-- ){
+            int[] curr = new int[ n + 1] ;
+            curr[n] = 1 ;;
+            for(int j = n - 1 ; j >= 0 ; j--){
+                char ch1 = sA[i] ;
+                char ch2 = tA[j] ;
 
-    public int helper(int i , int j){
-        if( j == tA.length) return 1 ;
-        if(i == sA.length || j == tA.length) return 0 ;
+                int result = 0 ;
+                if(ch1 == ch2){
+                    result += prev[j + 1] ;
+                }  
 
-        if(cache[i][j] == -1){
-            char ch1 = sA[i] ;
-            char ch2 = tA[j] ;
+                result += prev[j];
 
-            int result = 0 ;
-            if(ch1 == ch2){
-              result += helper(i + 1 , j + 1  ) ;
+                curr[j] = result ;
             }
-
-            result += helper(i + 1 , j) ;
-            cache[i][j] = result ;
+            prev = curr ;
         }
 
-        // System.out.print( "( " + i + " , " + j + " ) -> ") ;
-        // System.out.println(cache[i][j] ) ;
-
-        return cache[i][j] ;
+        return prev[0] ;
     }
+
+    // public int helper(int i , int j){
+    //     if( j == tA.length) return 1 ;
+    //     if(i == sA.length || j == tA.length) return 0 ;
+
+    //     if(cache[i][j] == -1){
+    //         char ch1 = sA[i] ;
+    //         char ch2 = tA[j] ;
+
+    //         int result = 0 ;
+    //         if(ch1 == ch2){
+    //           result += helper(i + 1 , j + 1  ) ;
+    //         }
+
+    //         result += helper(i + 1 , j) ;
+    //         cache[i][j] = result ;
+    //     }
+
+    //     return cache[i][j] ;
+    // }
 }
