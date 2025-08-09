fc72063e00324fbe4f6244f33e108a0c1007d4cf
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-08-09 22:55:07 +0530

    Time: 7 ms (46.22%) | Memory: 44.9 MB (33.88%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,30 +1,38 @@
 class Solution {
-    char[] s ;
-    Boolean[][] cache ;
     public int countSubstrings(String str) {
-        s = str.toCharArray() ;
-        int m = s.length ;
-        cache = new Boolean[m][m] ;
-        int count = 0 ;
-        for(int i = 0 ; i < m ; i++){
-            for(int j = i ; j < m ; j++){
-                if(isPalindrome(i , j)){
-                    count++ ;
-                }
+        char[] s = str.toCharArray();
+        int m = s.length;
+        boolean[] prev = new boolean[m] ;
+        int count = 0;
+        for (int i = m - 1; i >= 0; i--) {
+            boolean[] curr = new boolean[m];
+            curr[i] = true ;
+            count++ ;
+            for (int j = i + 1; j < m; j++) {
+                if(s[i] != s[j]) continue ;
+                
+                // equal
+                if(i + 1 == j) curr[j] = true ;
+                else curr[j] = prev[j-1] ;
+
+                if(curr[j]) count++ ;
             }
+            // System.out.println(Arrays.toString(curr)) ;
+            // System.out.println(count) ;
+            prev = curr ;
         }
-        return count ;
+        return count;
     }
 
-    public boolean isPalindrome(int i , int j){
-        if(i == j) return true ;
-        if(i > j) return true ;
+    // public boolean isPalindrome(int i , int j){
+    //     if(i == j) return true ;
+    //     if(i > j) return true ;
 
-        if(cache[i][j] == null){
-            if(s[i] == s[j]) cache[i][j] = isPalindrome(i + 1 , j - 1) ;
-            else cache[i][j] = false ;
-        }
+    //     if(cache[i][j] == null){
+    //         if(s[i] == s[j]) cache[i][j] = isPalindrome(i + 1 , j - 1) ;
+    //         else cache[i][j] = false ;
+    //     }
 
-        return cache[i][j] ;
-    }
+    //     return cache[i][j] ;
+    // }
 }
