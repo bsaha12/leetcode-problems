a11f66bcf6a6a8fa5e61ed185270ae4301947881
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-08-19 08:59:08 +0530

    Time: 16 ms (83.99%) | Memory: 42.9 MB (31.12%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.java
+++ 1511-count-number-of-teams/count-number-of-teams.java
@@ -1,20 +1,48 @@
 class Solution {
+    // int[] arr ;
     public int numTeams(int[] rating) {
+        // arr = rating ;
         int count = 0 ;
         int n = rating.length ;
+
+        int[] rightLesser = new int[n] ;
+        int[] rightGreater = new int[n] ;
+
+        int[] leftLesser = new int[n] ;
+        int[] leftGreater = new int[n] ;
         for(int i = 0 ; i < n ; i++){
-            int first = rating[i] ;
-            for(int j = i + 1 ; j < n ; j++){
-                int second = rating[j] ;
-                for(int k = j + 1 ; k < n ; k++){
-                    int third = rating[k] ;
-                    if(first < second && second < third){
-                        count++ ;
-                    }else if(first > second && second > third){
-                        count++ ;
-                    }
-                }
+            int curr = rating[i] ;
+
+            int leftSmall = 0 ;
+            int leftLarge = 0 ;
+            for(int j = 0 ; j < i ; j++){
+                if(rating[j] < curr) leftSmall++ ;
+                else leftLarge++ ;
             }
+            leftLesser[i] = leftSmall ;
+            leftGreater[i] = leftLarge ;
+
+            int rightSmall = 0 ;
+            int rightLarge = 0 ;
+            for(int j = i + 1 ; j < n ; j++){
+                if(rating[j] > curr) rightLarge++ ;
+                else rightSmall++ ;
+            }
+
+            rightLesser[i] = rightSmall ;
+            rightGreater[i] = rightLarge ;
+
+        }
+        for(int i = 1 ; i < n - 1 ; i++){
+            int mid = rating[i] ;
+
+            int rG = rightGreater[i] ;
+            int lL = leftLesser[i] ;
+
+            int rL = rightLesser[i] ;
+            int lG = leftGreater[i] ;
+
+            count += (rG * lL) + (rL * lG) ;
         }
 
         return count ;
