6efe6e089295a74a066a718ca7a129de5fcc9205
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-08-30 20:26:38 +0530

    Time: 28 ms (35.81%) | Memory: 45.2 MB (90.59%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -1,71 +1,123 @@
 class Solution {
-    int[][] grid ;
-    int row , col ;
-    int[][][] cache ;
+    // int[][] grid ;
+    // int row , col ;
+    // int[][][] cache ;
     public int cherryPickup(int[][] grid) {
-        this.grid = grid ;
-        row = grid.length ;
-        col = grid[0].length ;
-        cache = new int[ row + 1][col + 1][col + 1] ;
-        for(int[][] arr : cache){
-            for(int[] temp : arr){
-                Arrays.fill(temp , -1) ;
-            }
-        }
-        return helper(0 , col - 1  , 0) ;
-    }
-
-    public int helper(int x , int col1 , int col2){
-        // base condition
-        if( x >= row) return 0 ;
-
-        if(cache[x][col1][col2] != -1) return cache[x][col1][col2] ;
-
-        int curr = grid[x][col1] ;
-        if(col1 != col2){
-            curr += grid[x][col2] ;
-        }
-
-        int next = 0 ;
-        // next x is x + 1 
-        // find all possible cols which can be traversed
-
-        // right movement for robot 1
-        if(col1 + 1 < col){
-            next = Math.max(next , helper(x + 1 , col1 + 1 , col2) );
-            if(col2 + 1 < col)
-                next = Math.max(next , helper(x + 1 , col1 + 1 , col2 + 1));
-            if(col2 - 1 >= 0)
-                next = Math.max(next , helper(x + 1 , col1 + 1 , col2 - 1));
-        }
-
-        // right movement for robot 2
-        if(col2 + 1 < col){
-            next = Math.max(next , helper(x + 1 , col1 , col2 + 1) );
-            if( col1 - 1 >= 0)
-                next = Math.max(next , helper(x + 1 , col1 - 1 , col2 + 1));
-        }
-
-        if(col1 - 1 >= 0){
-            next = Math.max(next , helper(x + 1 , col1 - 1 , col2));
-            if(col2 - 1 >= 0){
-                next = Math.max(next , helper(x + 1 , col1 - 1 , col2 - 1)) ;
-            }
-        }
-
-        if(col2 -1 >= 0){
-            next = Math.max(next , helper(x + 1 , col1 , col2 - 1) ) ;
-        }
-
-        next = Math.max(next , helper( x + 1 , col1 , col2)) ;
-
-        // if( x == 2){
-        //     System.out.println("----Row : " + x + " --------" );
-        //     System.out.println("Col1 : " + col1 +  " , Col2 : " + col2) ;
-        //     System.out.println("Next : " + next + " , Curr : " + curr) ;
-        //     System.out.println("-----------") ;
+        // this.grid = grid ;
+        // row = grid.length ;
+        // col = grid[0].length ;
+        // cache = new int[ row + 1][col + 1][col + 1] ;
+        // for(int[][] arr : cache){
+        //     for(int[] temp : arr){
+        //         Arrays.fill(temp , -1) ;
+        //     }
         // }
-        
-        return cache[x][col1][col2] = next + curr ;
+        // return helper(0 , col - 1  , 0) ;
+
+        int row = grid.length;
+        int col = grid[0].length;
+        int[][] prev = new int[col][col];
+        for (int i = row - 1; i >= 0; i--) {
+            int[][] curr = new int[col][col];
+            for (int j = 0; j < col; j++) {
+                for (int k = 0; k < col; k++) {
+                    int currVal = grid[i][j];
+                    if (j != k) {
+                        currVal += grid[i][k];
+                    }
+                    int next = 0;
+                    int col1 = j;
+                    int col2 = k;
+
+                    // right movement for robot 1
+                    if (col1 + 1 < col) {
+                        next = Math.max(next, prev[col1 + 1][col2]);
+                        if (col2 + 1 < col)
+                            next = Math.max(next, prev[col1 + 1][col2 + 1]);
+                        if (col2 - 1 >= 0)
+                            next = Math.max(next, prev[col1 + 1][col2 - 1]);
+                    }
+
+                    // right movement for robot 2
+                    if (col2 + 1 < col) {
+                        next = Math.max(next, prev[col1][col2 + 1]);
+                        if (col1 - 1 >= 0)
+                            next = Math.max(next, prev[col1 - 1][col2 + 1]);
+                    }
+
+                    if (col1 - 1 >= 0) {
+                        next = Math.max(next, prev[col1 - 1][col2]);
+                        if (col2 - 1 >= 0) {
+                            next = Math.max(next, prev[col1 - 1][col2 - 1]);
+                        }
+                    }
+
+                    if (col2 - 1 >= 0) {
+                        next = Math.max(next, prev[col1][col2 - 1]);
+                    }
+
+                    next = Math.max(next, prev[col1][col2]);
+
+                    curr[j][k] = next + currVal;
+                }
+            }
+            prev = curr;
+        }
+
+        return prev[0][col - 1];
     }
+
+    // public int helper(int x , int col1 , int col2){
+    //     // base condition
+    //     if( x >= row) return 0 ;
+
+    //     if(cache[x][col1][col2] != -1) return cache[x][col1][col2] ;
+
+    //     int curr = grid[x][col1] ;
+    //     if(col1 != col2){
+    //         curr += grid[x][col2] ;
+    //     }
+
+    //     int next = 0 ;
+    //     // next x is x + 1 
+    //     // find all possible cols which can be traversed
+
+    //     // right movement for robot 1
+    //     if(col1 + 1 < col){
+    //         next = Math.max(next , helper(x + 1 , col1 + 1 , col2) );
+    //         if(col2 + 1 < col)
+    //             next = Math.max(next , helper(x + 1 , col1 + 1 , col2 + 1));
+    //         if(col2 - 1 >= 0)
+    //             next = Math.max(next , helper(x + 1 , col1 + 1 , col2 - 1));
+    //     }
+
+    //     // right movement for robot 2
+    //     if(col2 + 1 < col){
+    //         next = Math.max(next , helper(x + 1 , col1 , col2 + 1) );
+    //         if( col1 - 1 >= 0)
+    //             next = Math.max(next , helper(x + 1 , col1 - 1 , col2 + 1));
+    //     }
+
+    //     if(col1 - 1 >= 0){
+    //         next = Math.max(next , helper(x + 1 , col1 - 1 , col2));
+    //         if(col2 - 1 >= 0){
+    //             next = Math.max(next , helper(x + 1 , col1 - 1 , col2 - 1)) ;
+    //         }
+    //     }
+
+    //     if(col2 -1 >= 0){
+    //         next = Math.max(next , helper(x + 1 , col1 , col2 - 1) ) ;
+    //     }
+
+    //     next = Math.max(next , helper( x + 1 , col1 , col2)) ;
+
+    //     // if( x == 2){
+    //     //     System.out.println("----Row : " + x + " --------" );
+    //     //     System.out.println("Col1 : " + col1 +  " , Col2 : " + col2) ;
+    //     //     System.out.println("Next : " + next + " , Curr : " + curr) ;
+    //     //     System.out.println("-----------") ;
+    //     // }
+
+    //     return cache[x][col1][col2] = next + curr ;
+    // }
 }
