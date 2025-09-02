f9ff840c9ba469b3500bce88e03cd8c795fa40b6
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-09-02 21:14:25 +0530

    Time: 7 ms (73.18%) | Memory: 44.7 MB (68.94%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,58 +1,38 @@
 class Solution {
-    int[] coins ;
-    int[][] cache ;
     public int change(int amount, int[] coins) {
-        // Arrays.sort(coins) ;
-        this.coins = coins ;
-
-        // int[] dp = new int[amount + 1] ;
-        // dp[0] = 1 ;
-        
-        // for(int i = 1 ; i <= amount ; i++){
-        //     int result = 0 ;
-        //     for(int coin : coins){
-        //         if(i - coin >= 0){
-        //             result += dp[i - coin] ;
-        //         }
-        //     }
-        //     dp[i] = result ;
-        // }
-
-        // System.out.println(Arrays.toString(dp)) ;
-        // return dp[amount] ;
-
-        cache = new int[coins.length + 1][amount + 1];
-        for(int[] arr: cache){
-            Arrays.fill(arr , -1) ;
+        int[] prev = new int[amount + 1] ;
+        prev[0] = 1 ;
+        for(int start = coins.length - 1 ; start >= 0 ; start--){
+            int currVal = coins[start] ;
+            int[] curr = new int[amount + 1] ;
+            curr[0] = 1 ;
+            for(int i = 1 ; i <= amount ; i++){
+                int result = prev[i] ;
+                if(i - currVal >= 0){
+                    result += curr[i - currVal];
+                }
+                curr[i] = result ;
+            }
+            prev = curr ;
         }
-        return helper(0 , amount) ;
+        return prev[amount] ;
     }
 
-    public int helper(int start , int amount){
-    //     if(amount == 0) return 1 ;
+    // public int helper(int start , int amount){
 
-    //     int result = 0 ;
-    //     for(int coin : coins){
-    //         if(amount - coin >= 0){
-    //             result += helper(amount - coin) ;
-    //         }
+    //     if(amount == 0) return 1 ;
+    //     if(start >= coins.length) return 0 ;
+
+    //     if(cache[start][amount] != -1) return cache[start][amount] ;
+
+    //     int curr = coins[start] ;
+
+    //     int result = helper( start + 1 , amount) ;
+
+    //     if(amount - curr >= 0){
+    //         result += helper(start , amount - curr) ;
     //     }
 
-    //     return result ;
-
-        if(amount == 0) return 1 ;
-        if(start >= coins.length) return 0 ;
-
-        if(cache[start][amount] != -1) return cache[start][amount] ;
-
-        int curr = coins[start] ;
-
-        int result = helper( start + 1 , amount) ;
-
-        if(amount - curr >= 0){
-            result += helper(start , amount - curr) ;
-        }
-
-        return cache[start][amount] = result ;
-    }
+    //     return cache[start][amount] = result ;
+    // }
 }
