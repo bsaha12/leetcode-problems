6227be2e3b585c4005d1866149051f26cb0de1f7
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-09-02 08:46:34 +0530

    Time: 24 ms (9.47%) | Memory: 48.9 MB (57.75%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,58 @@
+class Solution {
+    int[] coins ;
+    int[][] cache ;
+    public int change(int amount, int[] coins) {
+        Arrays.sort(coins) ;
+        this.coins = coins ;
+
+        // int[] dp = new int[amount + 1] ;
+        // dp[0] = 1 ;
+        
+        // for(int i = 1 ; i <= amount ; i++){
+        //     int result = 0 ;
+        //     for(int coin : coins){
+        //         if(i - coin >= 0){
+        //             result += dp[i - coin] ;
+        //         }
+        //     }
+        //     dp[i] = result ;
+        // }
+
+        // System.out.println(Arrays.toString(dp)) ;
+        // return dp[amount] ;
+
+        cache = new int[coins.length + 1][amount + 1];
+        for(int[] arr: cache){
+            Arrays.fill(arr , -1) ;
+        }
+        return helper(0 , amount) ;
+    }
+
+    public int helper(int start , int amount){
+    //     if(amount == 0) return 1 ;
+
+    //     int result = 0 ;
+    //     for(int coin : coins){
+    //         if(amount - coin >= 0){
+    //             result += helper(amount - coin) ;
+    //         }
+    //     }
+
+    //     return result ;
+
+        if(amount == 0) return 1 ;
+        if(start >= coins.length) return 0 ;
+
+        if(cache[start][amount] != -1) return cache[start][amount] ;
+
+        int curr = coins[start] ;
+
+        int result = helper( start + 1 , amount) ;
+
+        if(amount - curr >= 0){
+            result += helper(start , amount - curr) ;
+        }
+
+        return cache[start][amount] = result ;
+    }
+}
