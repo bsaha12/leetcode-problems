a1105d5938e04f33bcb47680a88f5974a1f4d464
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-09-02 08:12:07 +0530

    Time: 14 ms (88.37%) | Memory: 44.4 MB (66.69%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,29 +1,47 @@
 class Solution {
-    int[] arr ;
-    Integer[][] cache ;
+    // int[] coins ;
     public int coinChange(int[] coins, int amount) {
         if(amount == 0) return 0 ;
-        arr = coins;
-        cache = new Integer[coins.length + 1 ][amount + 1] ;
-        return helper( 0 , amount) ;
+        // this.coins = coins ;
+        // int result =  helper(amount) ;
+        // return result == 0 ? -1 : result ;
+        int[] dp = new int[amount + 1] ;
+        dp[0] = 0 ;
+        for(int i = 1 ; i <= amount ; i++){
+            int result = 0 ;
+            for(int coin : coins){
+                if(i == coin){
+                    result = 1 ;
+                    break ;
+                }
+
+                if(i - coin > 0){
+                    int curr = dp[i - coin] ;
+                    if(curr > 0){
+                        result = result == 0 ? curr + 1 : Math.min(result , curr + 1) ;
+                    }
+                }
+            }
+            dp[i] = result ;
+        }
+
+        return dp[amount] == 0 ? -1 : dp[amount] ;
     }
 
-    public int helper(int i , int amount){
-        if(amount == 0) return 0 ;
-        if(amount < 0 || i >= arr.length) return -1 ;
-
-        if( cache[i][amount] != null ) return cache[i][amount] ;
-
-        int chosen = helper( i , amount - arr[i] ) ;
-        int nonChosen = helper(i + 1 , amount) ;
-
-        int ans = - 1 ;
-
-        if(chosen == -1 && nonChosen == -1) ans =  -1 ;
-        else if(chosen != -1 && nonChosen != -1) ans = Math.min(1 + chosen , nonChosen);
-        else if(chosen == -1) ans = nonChosen ;
-        else ans = 1 + chosen ;
-
-        return cache[i][amount] = ans  ;
-    }
+    // public int helper(int amount){
+    //     int result = 0 ;
+    //     for(int coin : coins){
+    //         if(amount == coin){
+    //             result = 1 ;
+    //             break ;
+    //         }
+    //         if(amount - coin > 0){
+    //             int curr = helper(amount - coin) ;
+    //             if(curr > 0){
+    //                 result = result == 0 ? curr + 1 : Math.min(result , curr + 1) ;
+    //             }
+    //         }
+    //     }
+    //     return result ;
+    // }
 }
