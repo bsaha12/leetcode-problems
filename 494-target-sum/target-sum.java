9aa6c6b7c9678d6d386b8082770572fdbc3c7abf
Bhaskar Saha <bhaskar12jan678@gmail.com>
2025-09-03 07:45:55 +0530

    Time: 11 ms (53.22%) | Memory: 44.6 MB (34.75%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,15 +1,33 @@
 class Solution {
+    int[] nums ;
+    int sum ;
+    int[][] cache ;
     public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums , 0 , 0 , target ) ;
+        this.nums = nums ;
+        int sum = Arrays.stream(nums).sum();
+        this.sum = sum ;
+        
+        int[][] cache = new int[nums.length + 1][sum*2 + 2] ;
+        for(int[] arr : cache){
+            Arrays.fill(arr , -1) ;
+        }
+        this.cache = cache ;
+        return helper(0 , target) ;
     }
-    public int helper(int[] nums , int i , int prevSum , int S){
-        // base condition
-        if(i == nums.length) return prevSum == S ? 1 : 0 ;
 
-        int curr = nums[i] ;
-        int positive = helper(nums , i + 1 , prevSum + curr , S) ;
-        int negative = helper(nums , i + 1 , prevSum - curr , S) ;
+    public int helper(int start , int target){
+        if(start == nums.length && target == 0) return 1 ;
+        if(start == nums.length) return 0 ;
+        if(Math.abs(target) > sum) return 0 ;
 
-        return positive + negative ;
+        int targetSumIndex = sum + target ;
+
+        // System.out.println("Sum : " + sum  + " , start : " + start + "")
+        if(cache[start][targetSumIndex] != -1) return cache[start][targetSumIndex] ;
+
+        int positive = helper(start + 1 , target - nums[start]) ;
+        int negative = helper(start + 1 , target + nums[start]) ;
+
+        return cache[start][targetSumIndex] = positive + negative ;
     }
 }
