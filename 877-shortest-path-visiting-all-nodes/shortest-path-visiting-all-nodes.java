8bcad167b157bf57b3c88baa627dd3c9cfdf5587
Bhaskar Saha <bhaskar12jan678@gmail.com>
2026-06-09 22:47:48 +0530

    Time: 9 ms (99.52%) | Memory: 46.3 MB (73.44%) - LeetSync

--- 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.java
+++ 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.java
@@ -1,38 +1,72 @@
 class Solution {
     public int shortestPathLength(int[][] graph) {
+    //     int n = graph.length;
+    //     Queue<State> q = new LinkedList<>();
+    //     Set<String> seen = new HashSet<>();
+
+    //     //put all nodes at queue
+    //     for(int i = 0 ; i < n ; i++){
+    //         Set<Integer> visited = new HashSet<>();
+    //         visited.add(i);
+    //         seen.add(i+"-"+visited);
+    //         q.offer(new State(i, visited));
+    //     }
+
+    //    int distance = 0;
+    //    while(!q.isEmpty()){
+    //     for(int i = q.size(); i > 0 ; i --){
+    //         State top = q.poll();
+
+    //         if(top.visited.size() == n) return distance;
+
+    //         for(int next: graph[top.node]){
+    //             Set<Integer> visited = new HashSet<>(top.visited);
+    //             visited.add(next);
+    //             String seenCondtion = next+"-"+visited ;
+    //             if(!seen.contains(seenCondtion)){
+    //                 seen.add(seenCondtion);
+    //                 q.offer(new State(next, visited));
+    //             }
+    //         }
+    //     }
+    //     distance++ ;
+    //    }
+    //    return -1;
+
         int n = graph.length;
-        Queue<State> q = new LinkedList<>();
-        Set<String> seen = new HashSet<>();
+        Queue<int[]> q = new LinkedList<>() ;
+        boolean[][] visited = new boolean[n][1 << n];
 
-        //put all nodes at queue
         for(int i = 0 ; i < n ; i++){
-            Set<Integer> visited = new HashSet<>();
-            visited.add(i);
-            seen.add(i+"-"+visited);
-            q.offer(new State(i, visited)) ;
+            q.offer(new int[]{i, 1 << i});
+            visited[i][1 << i] = true;
         }
 
-       int distance = 0;
-       while(!q.isEmpty()){
-        for(int i = q.size(); i > 0 ; i --){
-            State top = q.poll();
+        int distance = 0;
+        // BFS
+        while(!q.isEmpty()){
+            for(int i = q.size(); i > 0 ; i--){
+                int[] top = q.poll() ;
+                int node = top[0];
+                int maskedState = top[1];
 
-            if(top.visited.size() == n) return distance;
+                // check if all the node is visited,then return true
+                if(maskedState == (1 << n) - 1) return distance;
 
-            for(int next: graph[top.node]){
-                Set<Integer> visited = new HashSet<>(top.visited);
-                visited.add(next);
-                String seenCondtion = next+"-"+visited ;
-                if(!seen.contains(seenCondtion)){
-                    seen.add(seenCondtion);
-                    q.offer(new State(next, visited));
+                //visit neighbors if not already visited
+                for(int next: graph[node]){
+                    int nextMaskedState = maskedState | (1 << next) ;
+                    if(!visited[next][nextMaskedState]){
+                        visited[next][nextMaskedState] = true ;
+                        q.offer(new int[]{next, nextMaskedState });
+                    }
                 }
-            }
-        }
-        distance++ ;
-       }
 
-       return -1;
+            }
+            distance++;
+        }
+
+        return -1;
     }
 }
 
